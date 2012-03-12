95ae3e1d3c9606d34c84f29f4c8eff58b9dbe49b
Andreas Höhmann <andreas.hoehmann@mymita.com>
2012-03-12 19:55:27 +0100

    [intern] all xsd attributes with style "foo-bar" [add] support for gridlayout column2/row2 parameter [add] support for alignment and expandratio for ordered layouts

--- addon/src/main/java/com/mymita/vaadlets/addon/VaadletsBuilder.java
+++ addon/src/main/java/com/mymita/vaadlets/addon/VaadletsBuilder.java
@@ -7,16 +7,12 @@ import java.io.InputStream;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.mymita.vaadlets.Vaadlets;
-import com.mymita.vaadlets.layout.Alignment;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.ComponentContainer;
 
 public class VaadletsBuilder {
 
   private static final Log LOG = LogFactory.getLog(VaadletsBuilder.class);
-
-  private com.vaadin.ui.Component root;
 
   private static void addComponent(final com.vaadin.ui.Component vaadinParent,
       final com.mymita.vaadlets.core.Component vaadletParent, final com.vaadin.ui.Component vaadinComponent,
@@ -37,17 +33,53 @@ public class VaadletsBuilder {
     } else if (vaadinParent instanceof com.vaadin.ui.GridLayout) {
       final Short column = vaadletComponent.getColumn();
       final Short row = vaadletComponent.getRow();
-      if (column != null && row != null) {
+      final Short column2 = vaadletComponent.getColumn2();
+      final Short row2 = vaadletComponent.getRow2();
+      if (column != null && row != null && column2 != null && row2 != null) {
+        ((com.vaadin.ui.GridLayout) vaadinParent).addComponent(vaadinComponent, column, row, column2, row2);
+      } else if (column != null && row != null) {
         ((com.vaadin.ui.GridLayout) vaadinParent).addComponent(vaadinComponent, column, row);
       } else {
         ((com.vaadin.ui.GridLayout) vaadinParent).addComponent(vaadinComponent);
       }
     } else if (vaadinParent instanceof com.vaadin.ui.AbstractOrderedLayout) {
-      final Alignment alignment = vaadletComponent.getAlignment();
+      final com.mymita.vaadlets.layout.Alignment alignment = vaadletComponent.getAlignment();
       final Short expandRatio = vaadletComponent.getExpandRatio();
+      ((com.vaadin.ui.AbstractOrderedLayout) vaadinParent).addComponent(vaadinComponent);
+      if (alignment != null) {
+        ((com.vaadin.ui.AbstractOrderedLayout) vaadinParent).setComponentAlignment(vaadinComponent,
+            alignmentOf(alignment));
+      }
+      if (expandRatio != null) {
+        ((com.vaadin.ui.AbstractOrderedLayout) vaadinParent).setExpandRatio(vaadinComponent, expandRatio);
+      }
     } else {
       ((ComponentContainer) vaadinParent).addComponent(vaadinComponent);
     }
+  }
+
+  private static com.vaadin.ui.Alignment alignmentOf(final com.mymita.vaadlets.layout.Alignment aAlignment) {
+    switch (aAlignment) {
+      case BOTTOM_LEFT:
+        return com.vaadin.ui.Alignment.BOTTOM_LEFT;
+      case BOTTOM_CENTER:
+        return com.vaadin.ui.Alignment.BOTTOM_CENTER;
+      case BOTTOM_RIGHT:
+        return com.vaadin.ui.Alignment.BOTTOM_RIGHT;
+      case MIDDLE_LEFT:
+        return com.vaadin.ui.Alignment.MIDDLE_LEFT;
+      case MIDDLE_CENTER:
+        return com.vaadin.ui.Alignment.MIDDLE_CENTER;
+      case MIDDLE_RIGHT:
+        return com.vaadin.ui.Alignment.MIDDLE_RIGHT;
+      case TOP_LEFT:
+        return com.vaadin.ui.Alignment.TOP_LEFT;
+      case TOP_CENTER:
+        return com.vaadin.ui.Alignment.TOP_CENTER;
+      case TOP_RIGHT:
+        return com.vaadin.ui.Alignment.TOP_RIGHT;
+    }
+    throw new IllegalArgumentException(String.format("Unkown aligment '%s'", aAlignment));
   }
 
   private static void createChildComponents(final com.vaadin.ui.Component parent,
@@ -113,11 +145,11 @@ public class VaadletsBuilder {
     }
   }
 
+  private com.vaadin.ui.Component root;
+
   public void build(final InputStream inputStream) {
-    final Vaadlets v = JAXBUtils.unmarshal(inputStream);
-    final com.mymita.vaadlets.core.ComponentContainer r = v.getRootComponent();
-    root = createVaadinComponent(r);
-    createChildComponents(root, r);
+    final com.mymita.vaadlets.core.ComponentContainer r = JAXBUtils.unmarshal(inputStream).getRootComponent();
+    createChildComponents(root = createVaadinComponent(r), r);
   }
 
   public Component getRoot() {
